1946633761ce67bc637ef24871405e88e3805e38
dhira13 <92041833+dhira13@users.noreply.github.com>
2024-09-07 19:04:27 +0530

    update stop services

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-systemctl stop apache2
+
+# Check if the apache2 service is available (enabled or running)
+if systemctl list-units --full -all | grep -q "apache2.service"; then
+    # Stop the apache2 service
+    echo "Stopping apache2 service..."
+    sudo systemctl stop apache2
+    
+    # Verify if the service was stopped
+    if systemctl is-active --quiet apache2; then
+        echo "Failed to stop apache2 service."
+    else
+        echo "apache2 service stopped successfully."
+    fi
+else
+    echo "Service apache2 is not available."
+fi
